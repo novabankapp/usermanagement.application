7b57b0d640f3c10f4d3738f7c5e5b738b569188d
nbsdeveloper-lewis <75124231+nbsdeveloper-lewis@users.noreply.github.com>
2022-12-05 20:59:29 +0200

    Fixed verify otp

--- dtos/get_user_by_id_response.go
+++ dtos/get_user_by_id_response.go
@@ -1,8 +1,16 @@
 package dtos
 
-import "github.com/novabankapp/usermanagement.data/domain/registration"
+import (
+	"github.com/novabankapp/usermanagement.data/domain/registration"
+)
 
 type GetUserByIdResponse struct {
+	ID        string `json:"id"`
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+	UserName  string `json:"username"`
+	Email     string `json:"email"`
+	Phone     string
 }
 
 func GetResponseFromUser(user registration.User) *GetUserByIdResponse {
